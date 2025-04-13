a3b00630e1b512bb5cd44e6d46a64353aa624975
Gartonot <gartonot62@gmail.com>
2025-04-13 16:26:13 +0300

    Подправил внешний вид

--- public/script.js
+++ public/script.js
@@ -3,6 +3,6 @@ const socket = io();
 socket.on('chatMessage', (msg) => {
     const div = document.createElement('div');
     div.className = 'message';
-    div.innerHTML = `<strong>[${msg.source}] ${msg.user}:</strong> ${msg.message}`;
+    div.innerHTML = `<strong>[${msg.source}]</strong> ${msg.user}: ${msg.message}`;
     document.getElementById('chat').appendChild(div);
 });
