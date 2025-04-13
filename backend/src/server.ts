637e65fe3cafd3f1eaf041a74f524ef72e59f1cb
Gartonot <gartonot62@gmail.com>
2025-04-13 21:27:21 +0300

    Вынес url фронта в .env

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -13,7 +13,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new SocketIOServer<IServerToClientEvents>(server, {
     cors: {
-        origin: 'http://localhost:5173',
+        origin: process.env.FRONTEND_ORIGIN || 'http://localhost:5173',
         methods: ['GET', 'POST'],
     }
 });
