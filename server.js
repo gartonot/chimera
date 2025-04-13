7fec818ee38dc7ed709c8433440d3ed7c6ed6dce
Gartonot <gartonot62@gmail.com>
2025-04-13 16:32:03 +0300

    Добавил комменты для сервера

--- server.js
+++ server.js
@@ -1,48 +1,41 @@
-// server.js
 require('dotenv').config();
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
 const path = require('path');
 
+// Подключаем сервисы ютуба и твича
 const { startTwitchChat } = require('./twitch');
 const { startYouTubeChat } = require('./youtube');
 
-
+// Настройка сервера и вебсокетов
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
 
-// Отдача фронта
+// Отдача статики фронта
 app.use(express.static(path.join(__dirname, 'public')));
 
 // WebSocket
 io.on('connection', (socket) => {
     console.log('🟢 Клиент подключен');
 
-    socket.emit('chatMessage', {
-        source: 'chimera',
-        user: 'System',
-        message: 'Добро пожаловать в Chimera!',
-        timestamp: new Date().toISOString(),
-    });
-
     socket.on('disconnect', () => {
         console.log('🔴 Клиент отключен');
     });
 });
 
-// ▶️ Twitch-чат запускается после старта
+// ▶️ Twitch-чат запускается
 startTwitchChat(io);
 
+// ▶️ Youtube-чат запускается
+const YOUTUBE_STREAM_URL = 'https://www.youtube.com/watch?v=Q-ttbX02RWo';
+startYouTubeChat(io, YOUTUBE_STREAM_URL);
+
+
+// Запускаем сервер
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`🚀 Chimera запущен на http://localhost:${PORT}`);
 });
-
-
-// ▶️ Youtube-чат
-const YOUTUBE_STREAM_URL = 'https://www.youtube.com/watch?v=Xy6nHsUJrsc';
-
-startYouTubeChat(io, YOUTUBE_STREAM_URL);
