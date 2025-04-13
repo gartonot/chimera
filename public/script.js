f5c344a99feed608cc14281eca003ee34f1c56e3
Gartonot <gartonot62@gmail.com>
2025-04-13 17:31:21 +0300

    Добавил на фронте очередь сообщений + анимация

--- public/script.js
+++ public/script.js
@@ -1,8 +1,25 @@
 const socket = io();
 
+const chatEl = document.getElementById('chat');
+const messageQueue = [];
+const INTERVAL_DELAY = 100;
+
+// Приходящие сообщения от сервера
 socket.on('chatMessage', (msg) => {
-    const div = document.createElement('div');
-    div.className = 'message';
-    div.innerHTML = `<strong>[${msg.source}]</strong> ${msg.user}: ${msg.message}`;
-    document.getElementById('chat').appendChild(div);
+    messageQueue.push(msg);
 });
+
+// Каждые 300мс вытаскиваем одно сообщение из очереди
+setInterval(() => {
+    if (messageQueue.length > 0) {
+        const msg = messageQueue.shift();
+        showMessage(msg);
+    }
+}, INTERVAL_DELAY);
+
+function showMessage({ user, message, source }) {
+    const el = document.createElement('div');
+    el.className = 'chat-line';
+    el.innerHTML = `<b>[${source}] ${user}:</b> ${message}`;
+    chatEl.appendChild(el);
+}
